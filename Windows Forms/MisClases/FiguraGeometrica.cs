928516e02913fee83dadf916fd5d2357856e183e
MARIA PINEDO <MARIA PINEDO@MARIANGEL>
2024-03-02 23:31:46 -0400

    Agregar archivos de proyecto.

---
+++ Windows Forms/MisClases/FiguraGeometrica.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Forms.MisClases
+{
+    public class FiguraGeometrica
+    {
+        public string nombre;
+    }
+    public class Triangulo : Poligono
+    {
+        double Poligono.area(double valor1, double valor2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Rectangulo : Poligono
+    {
+        double Poligono.area(double valor1, double valor2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Paralelogramo : Poligono
+    {
+        double Poligono.area(double valor1, double valor2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Windows Forms/MisClases/Poligono.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Forms.MisClases
+{
+    public interface Poligono
+    {
+        double area(double valor1, double valor2);
+    }
+    
+}
